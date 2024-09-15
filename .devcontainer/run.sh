9910cb120b2489546be812a1855f66b01a2eb2d2
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2024-09-14 23:21:46 -0300

    feat(dev-env): add support for extra build and run flags

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -24,10 +24,52 @@ project_dir_path=$(
 	pwd -P
 )
 
+args=("$@")
+build_extra_flags=()
+run_extra_flags=()
+tmp_flags=()
+
+for arg in "${args[@]}"; do
+	if [ "$arg" == "--build" ]; then
+		run_extra_flags="${tmp_flags[@]}"
+		tmp_flags=()
+	elif [ "$arg" == "--run" ]; then
+		build_extra_flags="${tmp_flags[@]}"
+		tmp_flags=()
+	else
+		tmp_flags+=("$arg")
+	fi
+done
+
+case "${args[0]}" in
+"--run")
+	if [ -z "${run_extra_flags[@]}" ] && [ -z "${build_extra_flags[@]}" ]; then
+		run_extra_flags="${tmp_flags[@]}"
+	else
+		build_extra_flags="${tmp_flags[@]}"
+	fi
+	;;
+"--build")
+	if [ -z "${run_extra_flags[@]}" ] && [ -z "${build_extra_flags[@]}" ]; then
+		build_extra_flags="${tmp_flags[@]}"
+	else
+		run_extra_flags="${tmp_flags[@]}"
+	fi
+	;;
+"")
+	true
+	;;
+*)
+	echo "invalid options: $@"
+	echo "available commands are --build and --run"
+	exit 1
+	;;
+esac
+
 echo -e "build $image_name image\n"
-docker build $project_dir_path/.devcontainer/ -t $image_name || exit 1
+docker build $build_extra_flags $project_dir_path/.devcontainer/ -t $image_name || exit 1
 
 container_name="$image_name-$RANDOM"
-docker run --name $container_name -e $enviroment_flag -v $volume_flag -v $project_dir_path:/home/debian/kalkulilo -p 1234:1234 -d -it $image_name >/dev/null || exit 1
+docker run $run_extra_flags --name $container_name -e $enviroment_flag -v $volume_flag -v $project_dir_path:/home/debian/kalkulilo -p 1234:1234 -d -it $image_name >/dev/null || exit 1
 
 echo -e "\ncontainer $container_name created from $image_name image"
